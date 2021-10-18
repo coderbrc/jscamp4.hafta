08da2ad9a5f910fdfd7fb3f22f9b156474378cb8
burçak kasap <burcak_kasap92@hotmail.com>
2021-10-19 00:03:03 +0300

    19 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,19 @@
+console.log("Merhaba Kodlama.io")
+//js type safe değildir
+let dolarBugun=9.30
+
+let dolarDun=9.20
+{
+    let dolarDun=9.10
+}
+console.log(dolarDun)
+const euroDun=11.2
+console.log(euroDun)
+
+let users=["Kont kredisi","Emlak Kont Kredisi","Kam Kont Kredisi","Özel Kont Kredisi"]
+
+console.log("<ul>")
+for (let i = 0; i < users.length; i++) {
+        console.log("</list>"+users[i]+"</li>")
+}
+console.log("<ul>")
