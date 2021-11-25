cc24c35b691c07e5128358436a0cfa380d576832
burçak kasap <burcak_kasap92@hotmail.com>
2021-11-25 11:16:34 +0300

    .

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
+/*var,let,const
 console.log("Merhaba Kodlama.io")
 //js type safe değildir
 let dolarBugun=9.30
 
 let dolarDun=9.20
-{
+{ 
     let dolarDun=9.10
 }
 console.log(dolarDun)
@@ -16,4 +17,9 @@ console.log("<ul>")
 for (let i = 0; i < users.length; i++) {
         console.log("</list>"+users[i]+"</li>")
 }
-console.log("<ul>")
+console.log("<ul>")*/
+let sayi1=10;
+sayi1="Engin Demiroğ";
+let student={id:1,name:"Engin"}
+console.log(student);
+
